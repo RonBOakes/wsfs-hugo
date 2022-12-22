77ac216af68b88a88d2000efddad6ee4151816b7
Ron Oakes <ron@ron-oakes.us>
2022-12-22 12:37:46 -0700

    Update the headers for categoryDetail.php

--- website/admin/categoryDetail.php
+++ website/admin/categoryDetail.php
@@ -1,11 +1,12 @@
 <?PHP
 
-/* Written by Ronald B. Oakes, copyright  2015
+/* Written by Ronald B. Oakes, copyright  2015-2022
    Rights assigned to Worldcon Intellectual Property, A California Nonprofit Corporation
    For the exclusive of the World Science Fiction convention for purposes of administering the Hugo Awards
    All other uses are forbidden without explicit permission from the author and Worldcon Intellection Property.
 */
-
+/**
+*/
   require_once('library.php');
 
   $db = new database((isset($_POST['retro_hugo'])));
